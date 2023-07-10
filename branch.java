383953658a7ed5c8a0d260a548d2c625ffb2dc33
LokeshVasudevan <Lokeshloki1602@gmail.com>
2023-07-10 20:24:49 +0530

    commit

--- branch.java
+++ branch.java
@@ -1,7 +1,7 @@
-import java.io.*;
+
 public class branch
 {
-    public static void main()
+    public static void main (String args[])
     {
         System.out.println("branch java file");
     }
